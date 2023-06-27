13713838649466cd5d2b8b9297c27c52cc9a6069
Michal Gawlas <gawlas05@gmail.com>
2023-06-27 21:59:59 +0200

    Task 32.3 completed

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -14,9 +17,17 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    DbService dbService;
+
+    @Autowired
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage Your tasks");
+        functionality.add("Provides connection with Trello account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
@@ -25,6 +36,13 @@ public class MailCreatorService {
         //COMPANY DETAILS poniżej - użyłem tylko company name, ale to jednak jakieś DETAILS są
         context.setVariable("company_name", adminConfig.getCompanyName());
         context.setVariable("goodbye_message", "Thank You");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+
+
+        context.setVariable("tasks_quantity", dbService.getAllTasks().size());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
